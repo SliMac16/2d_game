916c422a072b5a172dab66d13198f0d3e6ca345c
SliMac16 <maciej.sliwinski98@gmail.com>
2023-01-20 16:47:10 +0100

    fixes

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -10,7 +10,7 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 4);
+        Destroy(gameObject, 10);
     }
 
     // Update is called once per frame
